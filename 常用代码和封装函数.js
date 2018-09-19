3e852542d91cf3c10b7ec60ac9e5caa5030781d7
ProsperLee <lcsshengsss@outlook.com>
2018-09-19 11:28:11 +0800

    1.0

--- 常用代码和封装函数.js
+++ 常用代码和封装函数.js
@@ -1,25 +1,3 @@
-
-
-/**
- * 滚动条
- */
-/*$(window).scroll(function () {
-    var scrollTop = $(this).scrollTop(); // 滚动条距离顶部的高度
-    var scrollHeight = $(document).height(); // 当前页面的总高度
-    var clientHeight = $(this).height(); // 当前可视的页面高度
-    // console.log("top:"+scrollTop+",doc:"+scrollHeight+",client:"+clientHeight);
-    if (scrollTop + clientHeight >= scrollHeight) { // 距离顶部+当前高度 >= 文档总高度 即代表滑动到底部         
-        alert('已经到底了！');
-    } else if (scrollTop <= 0) {
-        alert('已经到顶了！');
-    }
-});
-*/
-
-
-/*-----------------------------------------------------------------------------------------------------------------------------------*/
-
-
 /**
  * 获取星期几
  */
